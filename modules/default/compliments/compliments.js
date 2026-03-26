4734303166125706483e25c9f3e1a305821968db
kynguyen64241 <thanhnguyen64la@gmail.com>
2026-03-26 23:29:01 +0700

    Test 1

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -4,7 +4,7 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
+			anytime: ["Chào bạn nha!"],
 			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
 			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
 			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
