666bb0f1836d02cfecaa7353b775764a4b55728c
kynguyen64241 <thanhnguyen64la@gmail.com>
2026-03-26 23:31:05 +0700

    Test 2

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -7,7 +7,7 @@ Module.register("compliments", {
 			anytime: ["Chào bạn nha!"],
 			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
 			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
+			evening: ["Chào bạn nha!"],
 			"....-01-01": ["Happy new year!"]
 		},
 		updateInterval: 30000,
